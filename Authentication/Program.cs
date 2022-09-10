8ec06849bd760cec68f42798cc1555fb341ee81c
prabolenora@gmail.com <1007@Mitchi>
2022-09-10 21:15:46 +0530

    new packages

--- Authentication/Program.cs
+++ Authentication/Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
-
+app.MapGet("/authentication/{val}", (string val) =>
+{
+    return Results.Ok(val);
+});
 app.Run();
